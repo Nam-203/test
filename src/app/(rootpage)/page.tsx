962471ae6df47101217e7787917b667d71298475
Nam-203 <cuciuiu357@gmail.com>
2025-01-06 15:32:32 +0700

    upload

--- src/app/(rootpage)/page.tsx
+++ src/app/(rootpage)/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useCallback, useMemo } from "react";
 import { type ColDef } from "ag-grid-community";
 import { useRouter } from "next/navigation";
 
@@ -14,44 +15,53 @@ import { type TradingAccount } from "~/shared/types/typeProps";
 export default function MarketPage() {
 	const router = useRouter();
 
-	const columnDef: ColDef<TradingAccount>[] = [
-		{
-			field: "symbol",
-			headerName: "Symbol",
-			flex: 1,
-			cellRenderer: SymbolCellRenderer,
-			sortable: true,
-			filter: true,
-		},
-		{
-			field: "amount",
-			headerName: "Amount",
-			cellRenderer: AmountCellRenderer,
-			sortable: true,
-			filter: true,
-			width: 150,
-		},
-	];
+	const columnDef = useMemo<ColDef<TradingAccount>[]>(
+		() => [
+			{
+				field: "symbol",
+				headerName: "Symbol",
+				flex: 1,
+				cellRenderer: SymbolCellRenderer,
+				sortable: true,
+				filter: true,
+			},
+			{
+				field: "amount",
+				headerName: "Amount",
+				cellRenderer: AmountCellRenderer,
+				sortable: true,
+				filter: true,
+				width: 150,
+			},
+		],
+		[],
+	);
 
-	const handleRowClick = () => {
+	const handleRowClick = useCallback(() => {
 		router.push("/sign-up");
-	};
-	const getRowStyle = () => {
+	}, [router]);
+
+	const getRowStyle = useCallback(() => {
 		return { backgroundColor: "#143045", borderBottom: "0.1px solid #3e5366" };
-	};
+	}, []);
+
+	const defaultColDef = useMemo(
+		() => ({
+			flex: 1,
+			minWidth: 150,
+			resizable: true,
+			sortable: true,
+			filter: true,
+		}),
+		[],
+	);
 
 	return (
-		<div className="mt-[95px] h-screen bg-[#011d33]">
+		<div className="mt-[79px] h-screen bg-[#011d33]">
 			<AgGridComponent
 				data={accounts}
 				columnDefs={columnDef}
-				defaultColDef={{
-					flex: 1,
-					minWidth: 150,
-					resizable: true,
-					sortable: true,
-					filter: true,
-				}}
+				defaultColDef={defaultColDef}
 				handleClick={handleRowClick}
 				headerHeight={0}
 				getRowStyle={getRowStyle}
